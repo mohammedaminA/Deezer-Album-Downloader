75ee558b9d27e5074ac77f22cc460e966a76a58c
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2020-09-13 18:19:20 +0300

    Update Deezer Downloader.py

--- Deezer Downloader.py
+++ Deezer Downloader.py
@@ -30,6 +30,42 @@ def download_album(album_name):
         song = deezer.get_track(track_list[y])
         song["download"](download_dir, quality=track_formats.MP3_320)
         y += 1
+def download_playlist(playlist_name):
+    search_results = deezer.search_playlists(playlist_name, limit=5)
+    playlist_id = search_results[0]['id']
+    playlist = deezer.get_playlist(playlist_id)
+    number_songs = playlist['DATA']['NB_SONG']
+    download_dir = "/music1/" + playlist_name
+    songs = playlist['SONGS']
+    data = songs['data']
+    print(data)
+    song_id = []
+
+    x = 0
+    while x < number_songs:
+        song_id.append(data[9]['SNG_ID'])
+        x += 1
+
+    y = 0
+    while y < number_songs:
+        song = deezer.get_track(song_id[y])
+        song['download'](download_dir, quality=track_formats.MP3_320)
+        y = + 1
+
+
+def main(action=int(input(" Choose from the following: 1. Download an album 2. download a playlist : "))):
+    print(action)
+    if action == 1:
+        album_name = input(" Tell me the Album Name: ")
+        download_album(album_name)
+    elif action == 2:
+        playlist_name = input(" Tell me the playlist name : ")
+        download_playlist(playlist_name)
+    else:
+        print("You haven't selected a correct option")
+
+
+main()
 
 
 
